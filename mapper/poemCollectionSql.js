612ccc194c4c41eb78458414db76df05b253594b
1043893197@qq.com <1043893197@qq.com>
2020-04-23 11:00:43 +0800

    s

--- mapper/poemCollectionSql.js
+++ mapper/poemCollectionSql.js
@@ -4,13 +4,13 @@ const poemCollection = {
         all: "select * from  wudaici   limit ?,?",
         author: "select * from  wudaici where  author like ?   limit ?,?",
         rhythmic: "select * from  wudaici where  rhythmic like ?  limit ?,?",
-        type: "select * from  wudaici where   type = ? limit ?,?",
+        type: "select * from  wudaici where   type like ? limit ?,?",
     },
     listCount: {
         all: "select count(*) as total from wudaici ",
         author: "select count(*) as total from  wudaici where  author like ? ",
         rhythmic: "select count(*) as total from  wudaici where  rhythmic like ?  ",
-        type:  "select count(*) as total from  wudaici where  type = ? ",
+        type:  "select count(*) as total from  wudaici where  type like ? ",
         },
     poets: "select distinct author from wudaici ",
     rhythmic: "select distinct rhythmic from wudaici  ;"
