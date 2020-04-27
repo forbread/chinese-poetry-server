c7a5c2c6617e762292ac2c64200baaa54a72a963
chufeiqiang <1043893197@qq.com>
2020-04-27 15:51:36 +0800

    indexRouter

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
- 
- 
 
 var app = express();
 
@@ -21,7 +19,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/api/ancient-poetry', apiRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
