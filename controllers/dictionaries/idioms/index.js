95bbfde4466d41c07222a527401467ea51979af2
chu <chufeiqiang@hailiang.cn>
2020-12-03 10:01:40 +0800

    update:成语模糊查询

--- controllers/dictionaries/idioms/index.js
+++ controllers/dictionaries/idioms/index.js
@@ -2,23 +2,24 @@
 var mysql = require('mysql')
 var $confMysql = require('../../../config/dictionaries')
 var $sql = require('../../../mapper/dictionaries/idiomSql');
-let { getRequestParam,queryList } = require('../../../common/reqandresHandle')
+let { getRequestParam, queryList } = require('../../../common/reqandresHandle')
 
 // 使用连接池，提升性能
 var pool = mysql.createPool($confMysql);
 
 const poetryControllers = {
-   
-    getListByCondition(req, res, next) {
+
+    getListByCondition (req, res, next) {
         let param = getRequestParam(req)
-        let paramSql =["%" + param.word + "%", (param.page - 1) * param.size, param.size]
+        let likeParam = getLikeSql(param)
+        let paramSql = [likeParam, (param.page - 1) * param.size, param.size]
         let sqlCount = $sql.listCount.word
         let sqlStatement = $sql.list.word
         queryList(req, res, next, pool, { sqlCount, paramSql, sqlStatement }, param)
-        
+
     },
 
-    getList(req, res, next) {
+    getList (req, res, next) {
         let param = getRequestParam(req)
         let paramSql = [(param.page - 1) * param.size, param.size]
         let sqlCount = $sql.listCount.all
@@ -26,8 +27,26 @@ const poetryControllers = {
         queryList(req, res, next, pool, { sqlCount, paramSql, sqlStatement }, param)
     },
 
-    
+  
 
+
+
+}
+
+ function getLikeSql (param) {
+    let str = ''
+    if (param.num && parseInt(param.num) > 0) {
+
+
+
+        for (let i = 0; i < param.num; i++) {
+            str += '_'
+
+        }
+        return str + param.word + '%'
+    } else {
+        return '%' + param.word + '%'
+    }
 }
 
 
