085aa3857fa9cad835cb2c113d8caa8f6f869f84
chu <chufeiqiang@hailiang.cn>
2020-12-04 16:39:32 +0800

    切换阿里云环境

--- config/mysql.js
+++ config/mysql.js
@@ -1,10 +1,18 @@
 // MySQL数据库联接配置
-module.exports = {
-
-    host: '122.51.62.251',
+const cloundConfig = {
+    host: '8.135.39.65',
     user: 'root',
     password: 'Chu@666.',
     database: 'chinese-poetry',  
     port: 3306
+}
 
-};
+const localConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: 'chu123456',
+    database: 'chinese-poetry',  
+    port: 3306
+}
+
+module.exports = cloundConfig;
